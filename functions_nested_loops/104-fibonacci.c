26ea4a6785cf2cafb6744c20ecf00682719d02ee
iEmanHamdan <eman.hamdan@outlook.sa>
2026-03-29 09:08:35 +0000

    Advanced Task 16: Compress logic to pass Betty 40-line limit

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,45 +7,30 @@
  */
 int main(void)
 {
-	unsigned long f1_h = 0, f1_t = 1;
-	unsigned long f2_h = 0, f2_t = 2;
-	unsigned long n_h, n_t;
+	unsigned long f1_h = 0, f1_t = 1, f2_h = 0, f2_t = 2, n_h, n_t;
 	int i;
 
 	for (i = 1; i <= 98; i++)
 	{
 		if (f1_h == 0)
-		{
 			printf("%lu", f1_t);
-		}
 		else
-		{
 			printf("%lu%09lu", f1_h, f1_t);
-		}
-
-		if (i != 98)
-		{
-			printf(", ");
-		}
-		else
-		{
-			printf("\n");
-		}
+		
+		printf(i != 98 ? ", " : "\n");
 
 		n_t = f1_t + f2_t;
 		n_h = f1_h + f2_h;
-
 		if (n_t >= 1000000000UL)
 		{
 			n_h += 1;
 			n_t %= 1000000000UL;
 		}
-
+		
 		f1_h = f2_h;
 		f1_t = f2_t;
 		f2_h = n_h;
 		f2_t = n_t;
 	}
-
 	return (0);
 }
