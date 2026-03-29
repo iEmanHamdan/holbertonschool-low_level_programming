a85a223d9967fee1887efd693a7648ef201cebf5
iEmanHamdan <eman.hamdan@outlook.sa>
2026-03-29 10:01:37 +0000

    Task 7: Implement puts_half with correct odd/even logic

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, n, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+
+	for (i = len - n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
