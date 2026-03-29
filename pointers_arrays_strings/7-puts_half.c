e56cb6c3d980d88196aefba0ad47fcb5a110af1c
iEmanHamdan <eman.hamdan@outlook.sa>
2026-03-29 10:02:55 +0000

    Task 7: Adjust midpoint calculation to match desired output length

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,17 @@
  */
 void puts_half(char *str)
 {
-	int i, n, len = 0;
+	int i, len = 0;
 
 	while (str[len] != '\0')
 		len++;
 
-	if (len % 2 == 0)
-		n = len / 2;
-	else
-		n = (len + 1) / 2;
+	i = (len + 1) / 2;
 
-	for (i = len - n; i < len; i++)
+	while (str[i] != '\0')
+	{
 		_putchar(str[i]);
-
+		i++;
+	}
 	_putchar('\n');
 }
