789e7dbd0cfd0b8fff6c7d085deb1b593b2b512a
iEmanHamdan <eman.hamdan@outlook.sa>
2026-04-18 18:45:34 +0000

    Define dog_t as an alias for struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,7 +16,10 @@ struct dog
 	char *owner;
 };
 
-/* Add this prototype below the struct */
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
