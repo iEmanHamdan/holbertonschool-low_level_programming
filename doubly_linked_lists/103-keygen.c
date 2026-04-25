2be93b61ff8c06b1c57c1c9ccd7cf008a1813239
iEmanHamdan <eman.hamdan@outlook.sa>
2026-04-25 21:00:20 +0000

    Task 11: Add fully fixed keygen5 algorithm

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,61 +3,43 @@
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * main - generate a key depending on a username for crackme5
+ * @argc: Arguments Count
+ * @argv: Arguments Vector
+ * Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
 {
-	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcURVSQ1riXLIpeYAMjkPnOFR-_XJqcT";
-	int len, i, v1, v2, v3, v4;
-	char password[7];
-	char *name;
+	unsigned int i, b;
+	size_t len, ret;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *name, p[7] = "      ";
 
 	if (argc != 2)
 	{
-		printf("Usage: %s username\n", argv[0]);
+		printf("Correct usage: ./keygen5 username\n");
 		return (1);
 	}
-
 	name = argv[1];
 	len = strlen(name);
-
-	password[0] = l[(len ^ 59) & 63];
-
-	v1 = 0;
-	for (i = 0; i < len; i++)
-		v1 += name[i];
-	password[1] = l[(v1 ^ 79) & 63];
-
-	v2 = 1;
-	for (i = 0; i < len; i++)
-		v2 *= name[i];
-	password[2] = l[(v2 ^ 85) & 63];
-
-	v3 = name[0];
-	for (i = 0; i < len; i++)
-	{
-		if (name[i] > v3)
-			v3 = name[i];
-	}
-	srand(v3 ^ 14);
-	password[3] = l[rand() & 63];
-
-	v4 = 0;
-	for (i = 0; i < len; i++)
-		v4 += (name[i] * name[i]);
-	password[4] = l[(v4 ^ 239) & 63];
-
-	/* FIXED: Advance PRNG state, then call rand() again */
-	for (i = 0; i < name[0]; i++)
-		rand();
-	password[5] = l[(rand() ^ 229) & 63];
-
-	password[6] = '\0';
-
-	printf("%s", password);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, ret = 0; i < len; i++)
+		ret += name[i];
+	p[1] = l[(ret ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= name[i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = name[0], i = 0; i < len; i++)
+		if ((char)b < name[i])
+			b = name[i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += name[i] * name[i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < name[0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
 	return (0);
 }
