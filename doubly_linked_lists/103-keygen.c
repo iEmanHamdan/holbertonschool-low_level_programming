49d41fc0e0267a54816f0eaa6f72b8b3db8e8656
iEmanHamdan <eman.hamdan@outlook.sa>
2026-04-25 20:34:51 +0000

    Implement keygen for crackme5

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,20 +3,17 @@
 #include <string.h>
 
 /**
- * main - generates a valid key for crackme5 based on username
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * @argc: number of command-line arguments
- * @argv: array of command-line argument strings
- *
- * Return: 0 on success, 1 on error
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	unsigned int i;
-	unsigned int len;
-	unsigned int idx;
-	char *table = "0123456789abcdefghijklmnopqrstuvwxyz"
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ+-";
+	char password[7], *name;
+	int len = 0, i, v1, v2, v3, v4, v5;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcURVSQ1riXLIpeYAMjkPnOFR-_XJqcT";
 
 	if (argc != 2)
 	{
@@ -24,15 +21,47 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	len = strlen(argv[1]);
+	name = argv[1];
+	len = strlen(name);
 
+	/* Char 1: Length of username XOR 59 */
+	password[0] = l[(len ^ 59) & 63];
+
+	/* Char 2: Sum of ASCII values XOR 79 */
+	v1 = 0;
+	for (i = 0; i < len; i++)
+		v1 += name[i];
+	password[1] = l[(v1 ^ 79) & 63];
+
+	/* Char 3: Product of ASCII values XOR 85 */
+	v2 = 1;
+	for (i = 0; i < len; i++)
+		v2 *= name[i];
+	password[2] = l[(v2 ^ 85) & 63];
+
+	/* Char 4: Max char XOR 14, used to seed rand() */
+	v3 = name[0];
 	for (i = 0; i < len; i++)
 	{
-		idx = ((unsigned char)argv[1][i] ^ (i + len)) & 0x3f;
-		printf("%c", table[idx]);
+		if (name[i] > v3)
+			v3 = name[i];
 	}
+	srand(v3 ^ 14);
+	password[3] = l[rand() & 63];
 
-	printf("\n");
+	/* Char 5: Sum of squares of ASCII values XOR 239 */
+	v4 = 0;
+	for (i = 0; i < len; i++)
+		v4 += (name[i] * name[i]);
+	password[4] = l[(v4 ^ 239) & 63];
 
+	/* Char 6: Random seeded by first char XOR 229 */
+	for (i = 0; i < name[0]; i++)
+		v5 = rand();
+	password[5] = l[(v5 ^ 229) & 63];
+
+	password[6] = '\0';
+
+	printf("%s", password);
 	return (0);
 }
